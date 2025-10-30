eab456dacce26b23e24fb3f6a544664f1fc03939
JHONATAN MARCELINO DE JESUS <0201392411005>
2025-10-30 09:32:25 -0300

    Criação do controller e routes

--- VasosInteligentes/backend/src/index.ts
+++ VasosInteligentes/backend/src/index.ts
@@ -1,13 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-//importar as rotas
+import authRoutes from "./routes/authRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    credentials: true;
+    credentials: true,
     origin: "http://localhost:5173"
 }))
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-//app.use("/api", authRoutes)
+app.use("/api", authRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
